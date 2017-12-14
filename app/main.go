9ddcb2cea013d25147de407836fc65a54ba79239
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2017-12-14 13:03:34 -0200

    And we are done!

--- app/main.go
+++ app/main.go
@@ -17,10 +17,12 @@ func main() {
   defer c.Close()
   r := mux.NewRouter()
   r.HandleFunc("/", handle).Methods("GET")
+  fmt.Fprintln(os.Stdout, "Application started!")
   http.ListenAndServe(":" + os.Getenv("APPLICATION_PORT"), r)
 }
 
 func handle (w http.ResponseWriter, r *http.Request) {
   n, _ := c.Do("INCR", "acess")
+  fmt.Fprintln(os.Stdout, "Getting a request from:", r.RemoteAddr)
   fmt.Fprintf(w, "Hello World! I have been seen %v time(s).\n", n)
 }
