bc48fd6843a3f15e5c7ee9e91b34497a78f8637b
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2017-12-14 10:01:04 -0200

    All seams good

--- app/main.go
+++ app/main.go
@@ -3,32 +3,24 @@ package main
 import (
   "os"
   "fmt"
-  "time"
+  // "time"
   "net/http"
+
+  "github.com/gorilla/mux"
   "github.com/garyburd/redigo/redis"
 )
 
 var c redis.Conn
 
 func main() {
-  var err error
-  for trys := 5; trys > 0; trys++ {
-    c, err = redis.DialURL(os.Getenv("REDIS_URL"))
-    if err != nil {
-      fmt.Fprintf(os.Stderr, "Try %v failed dial to: %v\n", trys, os.Getenv("REDIS_URL"))
-      time.Sleep(5 * time.Second)
-    } else {
-      fmt.Fprintf(os.Stderr, "Redis initialized!\n")
-      break;
-    }
-  }
+  c, _ = redis.DialURL(os.Getenv("REDIS_URL"))
   defer c.Close()
-  http.HandleFunc("/", handle)
-  http.ListenAndServe(":" + os.Getenv("APPLICATION_PORT"), nil)
+  r := mux.NewRouter()
+  r.HandleFunc("/", handle).Methods("GET")
+  http.ListenAndServe(":" + os.Getenv("APPLICATION_PORT"), r)
 }
 
 func handle (w http.ResponseWriter, r *http.Request) {
-  // n, _ := c.Do("GET", "acess")
   n, _ := c.Do("INCR", "acess")
   fmt.Fprintf(w, "Hello World! I have been seen %v time(s).\n", n)
 }
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build --rm -t golangwebapp .
-
-docker run -p 8080:80 --name golangwebapp golangwebapp
